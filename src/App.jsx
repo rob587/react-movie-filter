1930d092869d4acc0d7b7ae2d610365314125226
rob587 <robertocamm02@yahoo.com>
2025-09-01 14:49:47 +0200

    select section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 
 const App = () => {
+
+  const filmList = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ];
+
+ const [choice, setChoice] = useState("")
+
+ const [genre, setGenre] = useState("")
+
   return (
-    <div>
-      
-    </div>
+
+    <select onChange={(e) => setGenre(e.target.value)} >
+      <option value="">Seleziona Genere</option>
+      <option value="">Fantascienza</option>
+      <option value="">Thriller</option>
+      <option value="">Romantico</option>
+      <option value="">Azione</option>
+      <option value="">Fantascienza</option>
+      <option value="">Thriller</option>
+    </select>
+  
   )
 }
 
